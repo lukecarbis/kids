15144e36de9bddbdcc6ad0a2fd5bd1e3b9a3fbbc
Luke Carbis <luke@carb.is>
2022-07-18 11:28:59 +1000

    Update openmoji shell script

--- scripts/shell/openmoji.sh
+++ scripts/shell/openmoji.sh
@@ -2,5 +2,5 @@
 
 openMojiRelease='https://github.com/hfg-gmuend/openmoji/releases/latest/download/openmoji-svg-color.zip'
 curl -L -o ./public/openmoji.zip $openMojiRelease
-unzip -o ./public/openmoji.zip -d ./public/openmoji
+unzip -oq ./public/openmoji.zip -d ./public/openmoji
 rm ./public/openmoji.zip
