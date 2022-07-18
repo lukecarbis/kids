129d66612994110484b5a2d06f93c5a9e63c0162
Luke Carbis <luke@carb.is>
2022-07-18 11:32:45 +1000

    Move assets back to static directory

--- scripts/shell/openmoji.sh
+++ scripts/shell/openmoji.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 openMojiRelease='https://github.com/hfg-gmuend/openmoji/releases/latest/download/openmoji-svg-color.zip'
-curl -L -o ./public/openmoji.zip $openMojiRelease
-unzip -oq ./public/openmoji.zip -d ./public/openmoji
-rm ./public/openmoji.zip
+curl -L -o ./static/openmoji.zip $openMojiRelease
+unzip -oq ./static/openmoji.zip -d ./static/openmoji
+rm ./static/openmoji.zip
