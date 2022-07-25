310c5c5038518b34be97a95c2cf1e90e223c98e0
Luke Carbis <luke@carb.is>
2022-07-26 09:14:47 +1000

    Disable create user

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -13,7 +13,7 @@ export async function post({ request }) {
 
 	try {
 		if (signUp) {
-			await createUserWithEmailAndPassword(auth, email, password);
+			// await createUserWithEmailAndPassword(auth, email, password);
 			// await updateProfile(auth.currentUser, { displayName: name });
 		} else {
 			await signInWithEmailAndPassword(auth, email, password);
