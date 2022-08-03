d89456d7019ae7ea21eb3292b7ce0e482a470016
Luke Carbis <luke@carb.is>
2022-08-03 16:44:34 +1000

    Move fetch list to single function

--- src/routes/[name].js
+++ src/routes/[name].js
@@ -1,24 +1,5 @@
-import { apiUrl } from '$lib/firebase';
+import { fetchList } from '$lib/list';
 
-export async function get({ locals, params }) {
-	if (!locals.loggedIn) {
-		return { status: 404 };
-	}
-
-	const { uid, idToken } = locals;
-
-	if (!uid || !idToken) {
-		return { status: 500 };
-	}
-
-	const result = await fetch(`${apiUrl}/${uid}/${params.name}.json?auth=${idToken}`);
-	const body = await result.json();
-
-	if (!body) {
-		return { status: 500 };
-	}
-
-	body.name = params.name;
-
-	return { status: 200, headers: {}, body };
+export async function get(request) {
+	return fetchList(request);
 }
