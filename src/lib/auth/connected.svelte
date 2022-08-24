77d42e72f661ba34ac74691829f03800c23f27d7
Luke Carbis <luke@carb.is>
2022-08-25 09:36:46 +1000

    Better connection dot

--- src/lib/auth/connected.svelte
+++ src/lib/auth/connected.svelte
@@ -6,22 +6,26 @@
 </script>
 
 <span
-	class="absolute w-2 h-2 rounded-full bottom-4 left-4"
-	class:bg-sky-400={connected}
-	class:border-2={!connected}
-	class:border-amber-400={!connected}
+	class="p-2 fixed bottom-2 left-4 rounded-lg"
 	on:mouseenter={() => (hover = true)}
 	on:mouseleave={() => (hover = false)}
-/>
-{#if hover}
-	<p
-		class="text-sm absolute bottom-2.5 left-8 text-slate-600"
-		transition:fly={{ x: -10, duration: 100 }}
-	>
-		{#if connected}
-			Connected
-		{:else}
-			Disconnected
-		{/if}
-	</p>
-{/if}
+>
+	<span
+		class="inline-block w-2 h-2 rounded-full"
+		class:bg-sky-400={connected}
+		class:border-2={!connected}
+		class:border-amber-400={!connected}
+	/>
+	{#if hover}
+		<p
+			class="inline-block text-sm text-slate-600 leading-4 relative top-px px-1"
+			transition:fly={{ x: -10, duration: 100 }}
+		>
+			{#if connected}
+				Connected
+			{:else}
+				Disconnected
+			{/if}
+		</p>
+	{/if}
+</span>
